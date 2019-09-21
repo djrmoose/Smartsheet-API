7d1789b56021da103b701741b36a3c2d4313a65e
djrmoose <55636305+djrmoose@users.noreply.github.com>
2019-09-21 13:00:32 -0700

    Add files via upload

---
+++ SmartsheetTest1/SmartsheetTest1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using System.Linq;
+
+
+namespace SmartsheetTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            String accessToken = "13jxiyjau34mt8snl33sh0zcz9";
+            SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
+
+            UserProfile userProfile = smartsheet.UserResources.GetCurrentUser();
+            Console.WriteLine(userProfile.FirstName);
+
+            PaginatedResult<Sheet> sheetsAvailable = smartsheet.SheetResources.ListSheets(null, null, null);
+            Console.WriteLine(sheetsAvailable.TotalCount);
+            Console.WriteLine(sheetsAvailable.Data[(int)sheetsAvailable.TotalCount - 1].Name);
+
+
+
+
+
+
+        }
+    }
+}
